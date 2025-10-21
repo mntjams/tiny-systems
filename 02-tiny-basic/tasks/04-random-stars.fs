f777cab1c061b460babe301cb391dda8c2d9f295
mntjams <matouszatloukal@seznam.cz>
2025-10-21 08:48:28 +0200

    Finish task 04 in TinyBASIC

--- 02-tiny-basic/tasks/04-random-stars.fs
+++ 02-tiny-basic/tasks/04-random-stars.fs
@@ -28,16 +28,29 @@ type Command =
 type State = 
   { Program : list<int * Command> 
     Variables : Map<string, Value> 
-    // TODO: You will need to include random number generator in the state!
+    Random : System.Random
     }
 
 // ----------------------------------------------------------------------------
 // Utilities
 // ----------------------------------------------------------------------------
 
-let printValue value = failwith "implemented in steps 1 and 3"
-let getLine state line = failwith "implemented in step 1"
-let addLine state (line, cmd) = failwith "implemented in step 2"
+let printValue value = 
+    match value with
+    | StringValue s -> printfn "%s" s
+    | NumberValue n -> printfn "%d" n
+    | BoolValue b -> printfn "%b" b
+
+let getLine state line =
+    let newl = state.Program |> List.tryFind(fun (l, _) -> l = line)
+    match newl with
+    | Some(l, cmd) -> l, cmd
+    | None -> failwith "no line found"
+
+let addLine state (line, cmd) = 
+  let newprog = state.Program |> List.filter(fun (l, _) -> line <> l) |> List.append([(line, cmd)]) |> List.sortBy(fun (l, _) -> l)
+  {state with Program = newprog}
+
 
 // ----------------------------------------------------------------------------
 // Evaluator
@@ -50,35 +63,94 @@ let binaryRelOp f args =
   | [NumberValue a; NumberValue b] -> BoolValue(f a b)
   | _ -> failwith "expected two numerical arguments"
 
-let rec evalExpression expr = 
+let rec evalExpression state expr = 
   // TODO: Add support for 'RND(N)' which returns a random number in range 0..N-1
   // and for binary operators ||, <, > (and the ones you have already, i.e., - and =).
   // To add < and >, you can use the 'binaryRelOp' helper above. You can similarly
   // add helpers for numerical operators and binary Boolean operators to make
   // your code a bit nicer.
-  failwith "implemented in steps 1 and 3"
+    match expr with
+    | Const c -> c 
+    | Function (s, el) ->
+        let v1 = evalExpression state el[0]
+        match s with
+        | "-" ->
+          let v2 = evalExpression state el[1]
+          match v1, v2 with
+          | NumberValue n1, NumberValue n2 -> NumberValue(n1 - n2)
+          | _ -> failwith "invalid arguments of function '-'"
+        | "=" ->
+          let v2 = evalExpression state el[1]
+          binaryRelOp (=) [v1; v2]
+        | "<" ->
+          let v2 = evalExpression state el[1]
+          binaryRelOp (<) [v1; v2]
+        | ">" ->
+          let v2 = evalExpression state el[1]
+          binaryRelOp (>) [v1; v2]
+        | "||" ->
+          let v2 = evalExpression state el[1]
+          match v1, v2 with
+          | BoolValue b1, BoolValue b2 -> BoolValue(b1 || b2)
+          | _ -> failwith "cannot apply || to non-bool value"
+        | "RND" -> 
+          let v1 = evalExpression state el[0]
+          match v1 with
+          | NumberValue n -> NumberValue(state.Random.Next n)
+          | _ -> failwith "RAND takes an argument of type NumberValue"
+        | _ -> failwith "unknown function name"
+    | Variable v -> state.Variables[v]
 
 let rec runCommand state (line, cmd) =
   match cmd with 
   | Run ->
       let first = List.head state.Program    
       runCommand state first
-
-  | Print(expr) -> failwith "implemented in step 1"
-  | Goto(line) -> failwith "implemented in step 1"
-  | Assign _ | If _ -> failwith "implemented in step 3"
-  
-  // TODO: Implement two commands for screen manipulation
-  | Clear | Poke _ -> failwith "not implemented"
-
-and runNextLine state line = failwith "implemented in step 1"
+  | Print(expr) ->
+      evalExpression state expr |> printValue
+      runNextLine state line
+  | Goto(line) ->
+      getLine state line |> runCommand state 
+  | Assign (s, e) ->
+    let new_ctx = state.Variables.Add(s, evalExpression state e)
+    let new_state = {state with Variables = new_ctx}
+    runNextLine new_state line
+  | If (e, c) ->
+    let v = evalExpression state e
+    match v with
+    | BoolValue(true) -> runCommand state (line, c)
+    | BoolValue(false) -> runNextLine state line
+    | _ -> failwith "cannot use non-bool expression in an 'if' statement"
+  | Clear ->
+    System.Console.Clear()
+    runNextLine state line
+  | Poke (x, y, e) ->
+    let vx = evalExpression state x
+    let vy = evalExpression state y
+    let ve = evalExpression state e
+    match vx, vy, ve with
+    | NumberValue nx, NumberValue ny, StringValue se ->
+      System.Console.SetCursorPosition(nx, ny)
+      System.Console.Write(se)
+      runNextLine state line
+    | _ -> failwith "incorrect POKE argument types"
+and runNextLine state line = 
+  let newl = state.Program |> List.tryFind(fun (l, _) -> l > line)
+  match newl with
+  | Some(l, cmd) -> runCommand state (l, cmd)
+  | None -> state
 
 // ----------------------------------------------------------------------------
 // Interactive program editing
 // ----------------------------------------------------------------------------
 
-let runInput state (line, cmd) = failwith "implemented in step 2"
-let runInputs state cmds = failwith "implemented in step 2"
+let runInput state (line, cmd) =
+  match line with
+  | Some ln -> addLine state (ln, cmd)
+  | None -> runCommand state (System.Int32.MaxValue, cmd)
+
+let runInputs state cmds =
+  List.fold (fun s (l, c) -> runInput s (l, c)) (state) cmds
 
 // ----------------------------------------------------------------------------
 // Test cases
@@ -100,7 +172,7 @@ let (.-) a b = Function("-", [a; b])
 let (.=) a b = Function("=", [a; b])
 let (@) s args = Function(s, args)
 
-let empty = { Program = []; Variables = Map.empty } // TODO: Add random number generator!
+let empty = { Program = []; Variables = Map.empty; Random = System.Random()} // TODO: Add random number generator!
 
 // NOTE: Random stars generation. This has hard-coded max width and height (60x20)
 // but you could use 'System.Console.WindowWidth'/'Height' here to make it nicer.
